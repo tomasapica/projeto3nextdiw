17b048b7acf94de7f82cc6c387f040f8bde3fca3
Tomás Amarelinho Pica <167481630+tomasapica@users.noreply.github.com>
2024-12-08 23:12:44 +0000

    Edited page.tsx and Created the components Header and Footer

---
+++ components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Header.module.css';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+        <h1>React & Next.js</h1>
+        <nav className={styles.nav}>
+            <ul>
+                <li>
+                    <Link href="/">Home →</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
