e636ef7ccad3cc860dae33cb5fc784320aa2eb5f
Tomás Amarelinho Pica <167481630+tomasapica@users.noreply.github.com>
2025-01-02 17:49:34 +0000

    Used hooks (useState ahd useEffects) to search Products and to add them to the "cart"

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -1,21 +1,85 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useSWR from 'swr'
 import { Product } from '../../models/interfaces'
 import ProdutosCard from '@/components/ProdutosCard';
 
 export default function ProdutosPage() {
 
+  //
+  // A. Gestão de Estados- useState
+  const [cart, setCart] = useState<Product[]>([])
+  const [search, setSearch] = useState("")
+  const [filteredData, setFilteredData] = useState<Product[]>([])
+
+
+  //
+  // B. Fetch de Dados - useSWR
   const fetcher = (url: string) => fetch(url).then(res => res.json());
   const { data, error, isLoading } = useSWR<Product[], Error>('api/products', fetcher)
 
+
+  //
+  // C. Transformação/processamento de Dados
+
+
+  //
+  // D. Funções utilitárias
+
+
+  //
+  // E. Handlers (interações do utilizador)
+  function addItemToCart(product: Product) {
+    setCart((prev) => // Assíncrono, logo devo usar useState e useEffect
+     [...prev, product]
+    )
+  }
+
+
+  //
+  // F. Efeitos - useEffect
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart))
+  }, [cart])
+
+  useEffect(() => {
+    const cart = localStorage.getItem("cart")
+    if(cart) {
+      setCart(JSON.parse(cart))
+    }
+  }, [])
+
+  useEffect(() => {
+
+    if (data) {
+      const newFilteredData = data.filter((p) => {
+        return p.title.toLowerCase().includes(search.toLowerCase())
+      })
+      setFilteredData(newFilteredData)
+    }
+
+  }, [search, data])
+
+
+  //
+  // G. Renderização de Componentes
   if (error) return <div>Error loading data</div>
   if (isLoading) return <div>Loading...</div>
   if (!data) return <div>No data!</div>
 
-  return <>
-    { data.map( p => (
+  return <section className="overflow-auto h-full flex flex-wrap justify-center">  
+
+    <section>
+      <h2>Pesquisa de Produtos</h2>
+      <input placeholder="Pesquisar"
+        value={search}
+        onChange={e => setSearch(e.target.value)} />
+    </section>
+
+    <section>
+      <h2>Selecione os seus Produtos</h2>
+      { filteredData.map( p => (
         <ProdutosCard
           key={p.id}
           id={p.id}
@@ -25,7 +89,31 @@ export default function ProdutosPage() {
           category={p.category}
           image={p.image}
           rating={p.rating}
-        />  
+          product={p}
+          addItemToCart={addItemToCart}
+          isSelected={cart.includes(p)}
+        />
       ))}
-  </>
+    </section>
+
+    <section>
+      <h2>Produtos Selecionados</h2>
+      {cart.map(p => (
+        <ProdutosCard
+          key={p.id}
+          id={p.id}
+          title={p.title}
+          price={p.price}
+          description={p.description}
+          category={p.category}
+          image={p.image}
+          rating={p.rating}
+          product={p}
+          addItemToCart={addItemToCart}
+          isSelected={true}
+        />
+      ))}
+    </section>
+
+  </section>
 }
--- components/ProdutosCard.tsx
+++ components/ProdutosCard.tsx
@@ -1,18 +1,39 @@
 import { Product } from '@/models/interfaces'
 import React from 'react'
 
-export default function ProdutosCard({ title, price, description, image, rating }: Product) {
-  return (
-    <article className="max-w-sm border border-gray-200 rounded-lg shadow-md p-4 bg-white mb-6">
-      <h3 className="text-lg font-bold text-gray-800 mb-2">{title}</h3>
-      <img
-        src={image}
-        alt="Imagem do produto"
-        className="w-full h-48 object-contain rounded-md mb-4"
-      />
-      <h4 className="text-lg font-semibold text-blue-600 mb-2">Preço: {price.toFixed(2)}€</h4>
-      <p className="text-sm text-gray-600">{description}</p>
-      <h5 className="text-sm text-blue-500">Rating: {rating.rate} Estrelas - {rating.count} Avaliações</h5>
-    </article>
-  )
+// props = properties/propriedades
+export interface ProductProps {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+      rate: number;
+      count: number;
+  }
+  product: Product
+  addItemToCart: (product: Product) => void;
+  isSelected: boolean;
+}
+
+export default function ProdutosCard({ title, price, description, image, rating, addItemToCart, isSelected }: ProductProps) {
+  return <article className={`max-w-sm border border-gray-200 rounded-lg p-4 ${isSelected ? 'bg-blue-100' : 'bg-white'} mb-6`}>
+    <h3 className="text-lg font-bold text-gray-800 mb-2">{title}</h3>
+    <img
+      src={image}
+      alt="Imagem do produto"
+      className="w-full h-40 object-contain rounded-md mb-4"
+    />
+    <h4 className="text-lg font-semibold text-blue-600 mb-2">Preço: {price.toFixed(2)}€</h4>
+    <p className={`rounded-md ${isSelected ? 'bg-white' : 'bg-gray-100'} text-sm text-gray-600`}>{description}</p>
+    <h5 className="text-sm text-blue-500">Rating: {rating.rate} Estrelas - {rating.count} Avaliações</h5>
+    <button 
+      className="bg-blue-400 rounded-md"
+      onClick={() => addItemToCart}
+      >
+        {isSelected ? "- Remover do Cesto" : "+ Adicionar ao Cesto"}
+    </button>
+  </article>
 }
